b5e00d08dbbc49631aa9faf3eca46ab093d5b920
Durgaprasad2468 <durgaprasad20072003@gmail.com>
2026-06-24 14:13:09 +0530

    reactcode

---
+++ myfirstapp/src/App.jsx
@@ -0,0 +1,15 @@
+//Functional Component
+
+function App() {
+  return (
+    <div>
+          <h1>Hello Everyone welcome to react</h1>
+          <h1>We are friends</h1>
+          
+
+    </div>
+    
+);
+}
+
+export default App;
